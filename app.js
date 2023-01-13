da95871811d223a89fdbde01ac4ee8d45f3af105
dhruv2253 <dhruv225@outlook.com>
2023-01-13 00:51:41 -0500

    linked files and query selected

---
+++ app.js
@@ -0,0 +1,6 @@
+const newButton = document.querySelector('.add');
+const book = document.querySelector('.book');
+const removeButton = document.querySelector('.remove');
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+
