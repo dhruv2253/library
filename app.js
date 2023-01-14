f75923dab87fe08cb85d1d15c68abd39ae2975bb
dhruv2253 <dhruv225@outlook.com>
2023-01-14 00:09:38 -0500

    created form and functionalities

--- app.js
+++ app.js
@@ -1,6 +1,37 @@
-const newButton = document.querySelector('.add');
-const book = document.querySelector('.book');
+const newButton = document.querySelector('.new');
+const bookD = document.querySelector('.book');
 const removeButton = document.querySelector('.remove');
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
+const titleD = document.querySelector('.title');
+const authorD = document.querySelector('.author');
+const addButton = document.querySelector('#sub');
+const form = document.getElementById('form');
+const titleinp = document.getElementById('title');
+const authorinp = document.getElementById('author');
+let myLibrary = [];
 
+function Book( titleP, authorP) {
+    title = titleP;
+    author = authorP;
+}
+
+function addBookToLibrary(){
+    event.preventDefault();
+    let titleP = titleinp.value;
+    let authorP = authorinp.value;
+    newBook = new Book(titleP, authorP);
+    myLibrary.push(newBook);
+    console.log(titleP);
+    console.info(myLibrary);
+}
+
+newButton.addEventListener('click', () => {
+    console.log("3");
+    if (form.style.display === 'none'){
+        form.style.display = 'block';
+    }
+    else {
+        form.style.display = 'none';
+    }
+});
+
+form.addEventListener('submit', addBookToLibrary);
